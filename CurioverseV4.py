32a2b7004aac33883cdcf6907a7d75b4afdba891
ArianaSS0 <ariana.simpkin@gmail.com>
2026-07-21 10:54:16 +1200

    C7 delete_curio shifted

--- CurioverseV4.py
+++ CurioverseV4.py
@@ -151,45 +151,9 @@ button_edit.pack(side="left", padx=12, pady=5)
 
 #...............................................................................................................
 #Button 3
-def delete_curio():
-    print("delete_curio Button pressed")
-     # Create popup to delete curio
-    popup3 = tk.Toplevel(root)
-    popup3.title("Delete Curio")
-    popup3.geometry("250x200") #Y,X
-    # Deleting curio code
-    tk.Label(popup3, text="Enter Curio ID:").pack(pady=5)
-    id_entry = tk.Entry(popup3)
-    id_entry.pack()
-    def confirm_delete():
-        try:
-            curio_id = int(id_entry.get())
-            cursor.execute("""
-                        DELETE FROM curios
-                        WHERE id = ?
-                        """, (curio_id,))
-            connection.commit()
-            if cursor.rowcount == 0: #If there are no curios print error basically
-                print("Error. No curio with that ID was found")
-            else:
-                print("Curio sucsessfully deleted.")
-        except ValueError:
-            print("Please enter a valid ID.")
-    #Delete button
-    tk.Button(popup3, text="Delete", command=confirm_delete).pack(pady=10)
-
-    button_delete = tk.Button(
-        footer,
-        font=("Arial", 12),
-        width=10,
-        height=1,
-        text="Delete Curio",
-        command=delete_curio
-    )
-    button_delete.pack(side="left", padx=12, pady=5)
-
-
-button_delete = tk.Button(footer, font = ("Arial",12), width = 10, height = 1, text = "Delete Curio", command=delete_curio)
+#Allows user to delete curios
+from delete_curioV4 import delete_curio
+button_delete = tk.Button(footer, font = ("Arial",12), width = 10, height = 1, text = "Delete Curio", command=lambda: delete_curio (root,cursor,connection,date_now,category_data))
 button_delete.pack(side="left", padx=12, pady=5)
 
 #...............................................................................................................
